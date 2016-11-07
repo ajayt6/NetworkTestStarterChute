8b2304d24e57eb1a277c9ac580f407f941747380
ajayt6 <ajayt6@yahoo.com>
2016-11-07 17:09:07 -0600

    Update run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -7,15 +7,15 @@
 # You can implement logic here to do periodic network tests and save the
 # results.  You can do that directly from this bash script or call another
 # program that you write (e.g. in Python).
-#while true; do
+while true; do
     
 	#
     #ping -c 5 www.google.com >> /usr/share/nginx/html/results.txt
     #sudo iptables -L >> /usr/share/nginx/html/results.txt
     #iperf -c iperf.wiscnet.net >> /usr/share/nginx/html/results.txt
     #
-    #sleep 60
-#done
+    sleep 60
+done
 
 # If execution reaches this point, the chute will stop running.
-#exit 0
+exit 0
