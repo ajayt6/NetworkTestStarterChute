38ac0d14b946458190b28fa51ab7476a4dc3cb1c
ajayt6 <ajayt6@yahoo.com>
2016-11-07 14:33:05 -0600

    Cleaning run.sh

--- chute/run.sh
+++ chute/run.sh
@@ -2,20 +2,20 @@
 
 # Start the web server.  Its static files are located in /usr/share/nginx/html/
 # if you want to change the content.
-/etc/init.d/nginx start
+#/etc/init.d/nginx start
 
 # You can implement logic here to do periodic network tests and save the
 # results.  You can do that directly from this bash script or call another
 # program that you write (e.g. in Python).
-while true; do
+#while true; do
     
 	#
-    ping -c 5 www.google.com >> /usr/share/nginx/html/results.txt
-    sudo iptables -L >> /usr/share/nginx/html/results.txt
-    iperf -c iperf.wiscnet.net >> /usr/share/nginx/html/results.txt
+    #ping -c 5 www.google.com >> /usr/share/nginx/html/results.txt
+    #sudo iptables -L >> /usr/share/nginx/html/results.txt
+    #iperf -c iperf.wiscnet.net >> /usr/share/nginx/html/results.txt
     #
-    sleep 60
-done
+    #sleep 60
+#done
 
 # If execution reaches this point, the chute will stop running.
 exit 0
